dbd3680dbbafb5049604b9e3282b0699233da58c
Akash Sahani <akash.sahani@ambab.com>
2021-11-11 18:55:22 +0530

    Data Binding

--- Ambab/EmiCalculator/view/frontend/web/js/emi-component.js
+++ Ambab/EmiCalculator/view/frontend/web/js/emi-component.js
@@ -2,7 +2,7 @@ define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
     'use strict';
     return Component.extend({
         defaults: {
-            template: 'Ambab_EmiCalculator/payment/knockout-test'
+            template: 'Ambab_EmiCalculator/payment/payment-form'
         },
         initialize: function () {
             this._super();
